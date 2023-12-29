19dcc8ebc9ef51fc4d85643515d7f71c0f69c932
Kviraj <kawa.135viraj@gmail.com>
2023-12-29 22:36:17 +0530

    feat: file upload form.

--- app/(dashboard)/(routes)/upload/page.tsx
+++ app/(dashboard)/(routes)/upload/page.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import UploadForm from "./_components/UploadForm";
 
 function Upload() {
-  return <div>Upload</div>;
+  return (
+    <div className="p-5 px-8 md:px-28 ">
+      <div className="flex justify-center items-center">
+        <h1
+          className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600
+        bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl"
+        >
+          Start uploading file and Share it.
+        </h1>
+      </div>
+      <div className="w-full h-full items-center mt-20">
+        <UploadForm />
+      </div>
+    </div>
+  );
 }
 
 export default Upload;
