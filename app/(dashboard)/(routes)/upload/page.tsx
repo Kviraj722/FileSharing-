be11eed35505725341ca0d1e4f079318e056aaf7
Kviraj <kawa.135viraj@gmail.com>
2024-01-02 10:03:51 +0530

    File upload bug fixes and upload the file to the db.

--- app/(dashboard)/(routes)/upload/page.tsx
+++ app/(dashboard)/(routes)/upload/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import UploadForm from "./_components/UploadForm";
-import { app } from "@/firebaseConfig";
+import { app, db } from "@/firebaseConfig";
 import {
   getDownloadURL,
   getStorage,
@@ -10,38 +10,78 @@ import {
 } from "firebase/storage";
 import ToasterSuccess from "@/app/_components/ToasterSuccess";
 import ToasterComponent from "@/app/_components/ToasterComponent";
+import {
+  addDoc,
+  collection,
+  doc,
+  getFirestore,
+  setDoc,
+} from "firebase/firestore";
+import { useUser } from "@clerk/nextjs";
+import { ClipLoader } from "react-spinners";
 
 function Upload() {
+  function generateUniqueRandomString(length: number): string {
+    const alphanumericChars =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let result = "";
+
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * alphanumericChars.length);
+      result += alphanumericChars.charAt(randomIndex);
+    }
+
+    return result;
+  }
+
+  const { user } = useUser();
   const [progress, setProgress] = useState<any>();
   const storage = getStorage(app);
-  const uploadFile = (file: any) => {
+  const [loading, setLoading] = useState(false);
+
+  const id = generateUniqueRandomString(6);
+  const uploadFile = async (file: any) => {
     try {
-      const metaData = {
-        contentType: file.type,
-      };
+      setLoading(true);
       const imageRef = ref(storage, "files/" + file?.name);
+
       const uploadTask = uploadBytesResumable(imageRef, file, file.type);
-      uploadTask.on("state_changed", (snapshort: any) => {
+      uploadTask.on("state_changed", async (snapshort: any) => {
         const progress =
           (snapshort.bytesTransferred / snapshort.totalBytes) * 100;
-        console.log("Upload is =>", progress, "%done");
-        setProgress(progress);
-        console.log("File uploaded successfully");
-        getDownloadURL(uploadTask.snapshot.ref).then((url) => {
-          console.log("URL =>", url);
-        });
+        console.log(progress);
       });
 
+      const fileSnapShot = await uploadTask;
+
+      const fileUrl = await getDownloadURL(fileSnapShot.ref);
+      console.log(fileUrl);
+      // const newDocRef = collection(db, "Files");
+      const newDocRef = doc(db, "Files", id);
+      const dataToUpload = {
+        id: id,
+        fileName: file.name,
+        fileSize: file.size,
+        fileType: file.type,
+        fileUrl: fileUrl,
+        email: user?.primaryEmailAddress?.emailAddress,
+        userName: user?.fullName,
+        password: "RandoMPassword",
+        shortUrl: `${process.env.NEXT_PUBLIC_BASE_URL}/${id}`,
+      };
+      const dataAdded = await setDoc(newDocRef, dataToUpload);
       ToasterSuccess("File uploaded successfully", 5000);
     } catch (err) {
       console.log("Error while uploading  =>>>>>>>>>>>>>>>>>>>>", err);
       ToasterComponent("Error while uploading the file", 5000);
     } finally {
-      // setFile(undefined)
+      setLoading(false);
     }
   };
   return (
+    
     <div className="p-5 px-8 md:px-28 ">
+
       <div className="flex justify-center items-center">
         <h1
           className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600
@@ -55,7 +95,17 @@ function Upload() {
           uploadBtnHandler={(file: any) => uploadFile(file)}
           progress={progress}
         />
+
       </div>
+      {loading && (
+          <div className="flex justify-center mt-4">
+            <ClipLoader
+              color="#4F46E5"
+              loading={loading}
+              size={45}
+            />
+          </div>
+        )}
     </div>
   );
 }
