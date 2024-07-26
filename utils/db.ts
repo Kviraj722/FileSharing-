40debec5fc738db476f06a6b24a1d1ac9bcaa58d
Kviraj <kawa.135viraj@gmail.com>
2024-07-26 14:55:45 +0530

    url check

--- utils/db.ts
+++ utils/db.ts
@@ -5,13 +5,13 @@ export const connectToDB = async () => {
   mongoose.set("strictQuery", true);
 
   if (isConnected) {
-    console.log("Mongodb is already connected");
+    console.log("Mongodb is already connected", process.env.MONGODB_URI);
     return;
   }
   try {
     await mongoose.connect(process.env.MONGODB_URI || "");
     isConnected = true;
-    console.log("MongoDB connected");
+    console.log("MongoDB connected", process.env.MONGODB_URI);
   } catch (e) {
     console.log("Error while connecting to the database", e);
   }
